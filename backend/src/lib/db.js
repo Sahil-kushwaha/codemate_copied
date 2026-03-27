ce152b77a04a42f473bcf71346f68c90c4f761c4
sahil kushwaha <sahilkushwaha5510@gmail.com>
2026-03-28 02:03:22 +0530

    fix: ensure both DB_URL and DB_URL_LOCAL are defined for MongoDB connection

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,10 +4,10 @@ import { ENV } from "./env.js";
 
 export const connectDB = async () => {
   try {
-    if (!ENV.DB_URL) {
+    if (!ENV.DB_URL || !ENV.DB_URL_LOCAL) {
       throw new Error("DB_URL is not defined in environment variables");
     }
-    const conn = await mongoose.connect(ENV.DB_URL);
+    const conn = await mongoose.connect(ENV.NODE_ENV === "production" ? ENV.DB_URL : ENV.DB_URL_LOCAL);
     console.log("✅ Connected to MongoDB:", conn.connection.host);
   } catch (error) {
     console.error("❌ Error connecting to MongoDB", error);
